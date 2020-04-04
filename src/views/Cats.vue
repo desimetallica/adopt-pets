663c21e0d63b136b5c80ab73a442beb08dca28a8
desimetallica <ddesi89@gmail.com>
2020-04-04 11:33:56 +0200

    added new files

---
+++ src/views/Cats.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Cats for adoption </h1>
+    <b-table striped hover :items="cats">
+      <!-- `data.value` is the value after formatted by the Formatter -->
+      <template v-slot:cell(name)="data">
+        <!-- `data.value` is the value after formatted by the Formatter -->
+        <router-link :to="`/pets/${data.index}`">
+          {{ data.value }}
+        </router-link>
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+import cats from '@/data/cats'
+
+export default {
+  data () {
+    return {
+      cats
+    }
+  }
+}
+</script>
